80e278588d89937d2f3734bb2e3de6225f8db796
ragulan <ragulanravi21@gmail.com>
2021-09-20 13:43:19 -0400

    adding the add new items to cart feature

--- src/reducers/cart_reducer.js
+++ src/reducers/cart_reducer.js
@@ -7,7 +7,30 @@ import {
 } from '../actions'
 
 const cart_reducer = (state, action) => {
-  return state
+  // handle add to cart action
+  if (action.type === ADD_TO_CART) {
+    // get values from the payload
+    const { id, color, amount, product } = action.payload
+    // need to check if item is already in the cart
+    // same item but maybe different colors
+    const tempItem = state.cart.find((i) => i.id === id + color)
+    if (tempItem) {
+    } else {
+      const newItem = {
+        id: id + color,
+        name: product.name,
+        color,
+        amount,
+        image: product.image[0].url,
+        price: product.price,
+        max: product.stock,
+      }
+      // overwrite the cart and add new item
+      return { ...state, cart: [...state.cart, newItem] }
+    }
+  }
+
+  // return state
   throw new Error(`No Matching "${action.type}" - action type`)
 }
 
