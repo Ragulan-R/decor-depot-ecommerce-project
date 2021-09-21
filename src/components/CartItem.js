56c71a38403735e5ae36443fbf62ca780ed385f7
ragulan <ragulanravi21@gmail.com>
2021-09-21 11:27:11 -0400

    cart page layout completed

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -4,8 +4,39 @@ import { formatPrice } from '../utils/helpers'
 import AmountButtons from './AmountButtons'
 import { FaTrash } from 'react-icons/fa'
 import { useCartContext } from '../context/cart_context'
-const CartItem = () => {
-  return <h4>cart item</h4>
+
+// passing everything from cart item so need to desctructure
+const CartItem = ({ id, image, name, color, price, amount }) => {
+  const { removeItem, toggleAmount } = useCartContext()
+  // increase and decrease amount
+  const increase = () => {}
+  const decrease = () => {}
+  return (
+    <Wrapper>
+      {/* column 1 */}
+      <div className='title'>
+        <img src={image} alt={name} />
+        <div>
+          <h5 className='name'>{name}</h5>
+          <p className='color'>
+            color : <span style={{ background: color }}></span>
+          </p>
+          <h5 className='price-small'>{formatPrice(price)}</h5>
+        </div>
+      </div>
+      {/* column 2 */}
+      <h5 className='price'>{formatPrice(price)}</h5>
+      <AmountButtons amount={amount} increase={increase} decrease={decrease} />
+      <h5 className='subtotal'>{formatPrice(price * amount)}</h5>
+      <button
+        type='button'
+        className='remove-btn'
+        onClick={() => removeItem(id)}
+      >
+        <FaTrash />
+      </button>
+    </Wrapper>
+  )
 }
 
 const Wrapper = styled.article`
